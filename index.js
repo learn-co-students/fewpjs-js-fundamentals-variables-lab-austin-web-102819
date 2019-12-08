482cac64967df3cff6a8c5dace51fa7570cd4b03
fmwong77 <fongmeiw@gmail.com>
2019-12-08 00:11:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+// const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Upper West Side";
+mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
+// companyCEO = "Susan Smith";
